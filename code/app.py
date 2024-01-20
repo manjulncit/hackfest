2c3a5fa4dd8e2d3f691b14cca3de10c24821962d
Manjul Bhattarai <manjul.bhattarai@securin.io>
2024-01-20 08:31:38 +0545

    feat: integrated blueprint to have isolation

--- code/app.py
+++ code/app.py
@@ -1,63 +1,9 @@
 from flask import Flask, render_template, request, redirect
- 
-from core.service.user_service import UserService
-
-import mysql.connector
 
 app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return render_template('pages/home.html')
- 
-@app.route('/login')
-def login():
-    return render_template('pages/login.html')
 
-@app.route('/register')
-def register():
-    return render_template('pages/register.html')
-
-@app.route('/forgot-password')
-def forgot_password():
-    return render_template('pages/forgot-password.html')
-
-@app.route('/doLogin',methods=['POST'])
-def do_login():
-    user_service = UserService()
-
-    email = request.form['email']
-    password = request.form['password']
-
-    res = user_service.check_login(email,password)
-    
-    if res.get("status"):
-        return redirect('/')
-    else:
-        return redirect('/login')
-
-@app.route('/doRegister',methods=['POST'])
-def do_register():
-    user_service = UserService()
-
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    email = request.form['email']
-    password = request.form['password']
-
-    data = {"first_name": first_name,"last_name":last_name,"email":email,"password":password}
-    res = user_service.register_user(data)
-    print(res)
-    if res.get("status"):
-        return redirect('/login')
-    else:
-        return redirect('/register')
-
-@app.route('/testdb')
-def test_db():
-    user_service = UserService()
-    res = user_service.get_all_users()
-    return res
+from core.routes.users import users
+app.register_blueprint(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ code/core/routes/users.py
@@ -0,0 +1,54 @@
+from flask import Blueprint,request,render_template,redirect
+
+from core.service.user_service import UserService
+
+import mysql.connector
+
+users = Blueprint('users', __name__)
+
+@users.route('/')
+def hello_world():
+    return render_template('pages/home.html')
+ 
+@users.route('/login')
+def login():
+    return render_template('pages/login.html')
+
+@users.route('/register')
+def register():
+    return render_template('pages/register.html')
+
+@users.route('/forgot-password')
+def forgot_password():
+    return render_template('pages/forgot-password.html')
+
+@users.route('/doLogin',methods=['POST'])
+def do_login():
+    user_service = UserService()
+
+    email = request.form['email']
+    password = request.form['password']
+
+    res = user_service.check_login(email,password)
+    
+    if res.get("status"):
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+@users.route('/doRegister',methods=['POST'])
+def do_register():
+    user_service = UserService()
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+
+    data = {"first_name": first_name,"last_name":last_name,"email":email,"password":password}
+    res = user_service.register_user(data)
+    print(res)
+    if res.get("status"):
+        return redirect('/login')
+    else:
+        return redirect('/register')
